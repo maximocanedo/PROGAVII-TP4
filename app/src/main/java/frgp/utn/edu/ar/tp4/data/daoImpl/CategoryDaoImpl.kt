38e12be017782fcc7dfe40a0942790e0f650c7f0
lucas <lucasgruden0@protonmail.com>
2024-10-06 07:01:08 -0300

    Actualización de CategoryDao y CategoryDaoImpl

--- app/src/main/java/frgp/utn/edu/ar/tp4/data/daoImpl/CategoryDaoImpl.kt
+++ app/src/main/java/frgp/utn/edu/ar/tp4/data/daoImpl/CategoryDaoImpl.kt
@@ -2,80 +2,82 @@ package frgp.utn.edu.ar.tp4.data.daoImpl
 
 import frgp.utn.edu.ar.tp4.data.dao.CategoryDao
 import frgp.utn.edu.ar.tp4.data.models.Category
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.sql.Connection
 import java.sql.DriverManager
 import java.sql.ResultSet
 import java.sql.Statement
 
 class CategoryDaoImpl : CategoryDao {
-    override fun insertCategory(category: Category) {
-        TODO("Not yet implemented")
-    }
 
-    override fun updateCategory(category: Category) {
-        TODO("Not yet implemented")
-    }
-
-    override fun deleteCategory(id: Int): Boolean {
+    override suspend fun deleteCategory(id: Int): Boolean {
         val dataDB = DataDB()
         var deleted = false
-        try {
-            val connection: Connection = DriverManager.getConnection(dataDB.urlMySQL, dataDB.user, dataDB.pass)
-            val statement: Statement = connection.createStatement()
-
-            val rowCount = statement.executeUpdate("DELETE FROM categoria WHERE id = ${id}")
-
-            deleted = rowCount > 0
-
-            statement.close()
-            connection.close()
-        }
-        catch (e: Exception) {
-            e.printStackTrace()
+        withContext(Dispatchers.IO) {
+            try {
+                Class.forName(dataDB.driver)
+                val connection: Connection = DriverManager.getConnection(dataDB.urlMySQL, dataDB.user, dataDB.pass)
+                val statement: Statement = connection.createStatement()
+                val result = statement.executeUpdate("DELETE FROM categoria WHERE id = ${id}")
+                if (result > 0) {
+                    deleted = true
+                }
+                statement.close()
+                connection.close()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
         return deleted
     }
 
-    override fun getCategoryById(id: Int): Category {
-        val category = Category();
+    override suspend fun getCategoryById(id: Int): Category {
+        val category = Category()
         val dataDB = DataDB()
-        try {
-            val connection: Connection = DriverManager.getConnection(dataDB.urlMySQL, dataDB.user, dataDB.pass)
-            val statement: Statement = connection.createStatement()
-            val resulSet: ResultSet = statement.executeQuery("SELECT * FROM categoria")
+        withContext(Dispatchers.IO) {
+            try {
+                Class.forName(dataDB.driver)
+                val connection: Connection = DriverManager.getConnection(dataDB.urlMySQL, dataDB.user, dataDB.pass)
+                val statement: Statement = connection.createStatement()
+                val resulSet: ResultSet = statement.executeQuery("SELECT * FROM categoria WHERE id = ${id}")
 
-            category.setId(resulSet.getInt("id"))
-            category.setDescription(resulSet.getString("descripcion"))
-
-            resulSet.close()
-            statement.close()
-            connection.close()
+                if (resulSet.next()) {
+                    category.setId(resulSet.getInt("id"))
+                    category.setDescription(resulSet.getString("descripcion"))
+                }
+                resulSet.close()
+                statement.close()
+                connection.close()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
-        catch (e: Exception) {
-            e.printStackTrace()
-        }
-        return category;
+        return category
     }
 
     override suspend fun getAllCategories(): List<Category> {
         val categories : MutableList<Category> = mutableListOf<Category>().toMutableList()
         val dataDB = DataDB()
-        try {
-            val connection: Connection = DriverManager.getConnection(dataDB.urlMySQL, dataDB.user, dataDB.pass)
-            val statement: Statement = connection.createStatement()
-            val resulSet: ResultSet = statement.executeQuery("SELECT * FROM categoria")
+        withContext(Dispatchers.IO) {
+            try {
+                Class.forName(dataDB.driver)
+                val connection: Connection = DriverManager.getConnection(dataDB.urlMySQL, dataDB.user, dataDB.pass)
+                val statement: Statement = connection.createStatement()
+                val resulSet: ResultSet = statement.executeQuery("SELECT * FROM categoria")
 
-            while (resulSet.next()) {
-                val category = Category()
-                category.setId(resulSet.getInt("id"))
-                category.setDescription(resulSet.getString("descripcion"))
-                categories += category
+                while (resulSet.next()) {
+                    val category = Category()
+                    category.setId(resulSet.getInt("id"))
+                    category.setDescription(resulSet.getString("descripcion"))
+                    categories.add(category)
+                }
+                resulSet.close()
+                statement.close()
+                connection.close()
+            } catch (e: Exception) {
+                e.printStackTrace()
             }
-            resulSet.close()
-            statement.close()
-            connection.close()
-        } catch (e: Exception) {
-            e.printStackTrace()
         }
         return categories
     }
