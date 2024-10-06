074d0245e854cd576c9dbb5a6a2989816c74f410
carlo-renosto <carlorenosto13@gmail.com>
2024-10-06 04:12:49 -0300

    Añadir atributos Text, Msg y Error

--- app/src/main/java/frgp/utn/edu/ar/tp4/activity/Article/ArticleViewModel.kt
+++ app/src/main/java/frgp/utn/edu/ar/tp4/activity/Article/ArticleViewModel.kt
@@ -1,5 +1,8 @@
 package frgp.utn.edu.ar.tp4.activity.Article
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import frgp.utn.edu.ar.tp4.data.dao.ArticleDao
@@ -7,6 +10,20 @@ import frgp.utn.edu.ar.tp4.data.models.Article
 import kotlinx.coroutines.launch
 
 class ArticleViewModel(private val articleDao: ArticleDao) : ViewModel() {
+    var create__nameText by mutableStateOf("")
+    var create__stockText by mutableStateOf("")
+    var create__categoryText by mutableStateOf("")
+
+    var create__nameMsg by mutableStateOf("")
+    var create__stockMsg by mutableStateOf("")
+    var create__nameError by mutableStateOf(false)
+    var create__stockError by mutableStateOf(false)
+
+    fun updateFieldsFromArticle(article: Article) {
+        create__nameText = article.getName()
+        create__stockText = article.getStock().toString()
+        create__categoryText = article.getCategory().getDescription()
+    }
 
     fun insertArticle(article: Article) {
         viewModelScope.launch {
